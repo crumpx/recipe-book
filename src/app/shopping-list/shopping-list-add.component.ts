0fed12f45f7803ba15ae876a963afb4ac5152e0f
crumpx <crumpx@gmail.com>
2016-08-16 18:22:27 -0400

    next step to add forms, #96 current

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from "@angular/core";
+import {Ingredient} from "../shared/ingredient";
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: 'shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  isAdd = true;
+  @Input() item: Ingredient;
 
-  constructor() { }
+
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(ingredient: Ingredient){
+    if(!this.isAdd) {
+      //edit
+    } else {
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+    }
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {ShoppingListAddComponent} from "./shopping-list-add.component";
 import {Ingredient} from "../shared/ingredient";
 import {ShoppingListService} from "./shopping-list.service";
@@ -10,11 +10,17 @@ import {ShoppingListService} from "./shopping-list.service";
 })
 export class ShoppingListComponent implements OnInit {
   items: Ingredient[] = [];
+  selectedItem: Ingredient = null;
 
   constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
     this.items = this.sls.getItems();
+  }
+
+  onSelectItem(item: Ingredient) {
+    this.selectedItem = item;
+
   }
 
 }
